96f300ccc22bb60f95daceeec3db69ad4d969c75
yungblud <killi8n@gmail.com>
2022-04-27 20:47:49 +0900

    feat: added Seekbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,56 @@ const ToolBox = styled.div`
 
     border-radius: 8px;
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+
+    padding: 24px;
+`
+
+const SeekBarLayout = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const CircleButton = styled.button`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    font-size: 28px;
+    font-weight: bold;
+    background-color: #ffffff;
+    border: 1px solid #868e96;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #adb5bd;
+    }
+    &:active {
+        background-color: #495057;
+        color: #ffffff;
+    }
+`
+
+const SeekBar = styled.div`
+    flex: 1;
+    background-color: #343a40;
+    height: 5.5px;
+    margin-left: 16px;
+    margin-right: 16px;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+`
+
+const Seeker = styled.span`
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background-color: #ffffff;
+    z-index: 50;
+    cursor: pointer;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `
 
 const Home: NextPage = () => {
@@ -28,6 +78,15 @@ const Home: NextPage = () => {
     return (
         <Layout>
             <ToolBox>
+                <SeekBarLayout>
+                    <CircleButton>-</CircleButton>
+                    <SeekBar>
+                        <Seeker />
+                    </SeekBar>
+                    <CircleButton style={{ marginLeft: 'auto' }}>
+                        +
+                    </CircleButton>
+                </SeekBarLayout>
                 {/* <button
                     type="button"
                     onClick={() => {
