a670b10ddc422c98067e6b470a769bd869179929
yungblud <killi8n@gmail.com>
2022-04-27 21:43:29 +0900

    feat: added eventlistener

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import styled from '@emotion/styled'
 import type { NextPage } from 'next'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Layout = styled.main`
     min-width: 100vw;
@@ -71,17 +71,82 @@ const Seeker = styled.span`
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `
 
+const BpmMarker = styled.h1`
+    font-weight: bold;
+    font-size: 44px;
+    width: 100%;
+    text-align: center;
+`
+
+const MINIMUM_BPM = 20
+const MAXIMUM_BPM = 240
+
 const Home: NextPage = () => {
     const audioRef = useRef<HTMLAudioElement>(null)
-    useEffect(() => {}, [])
+    const seekerRef = useRef<HTMLSpanElement>(null)
+    const seekBarRef = useRef<HTMLDivElement>(null)
+    const [isSeeking, setIsSeeking] = useState<boolean>(false)
+    const [seekerLeftPercentage, setSeekerLeftPercentage] = useState<number>(50)
+    const [bpm, setBpm] = useState<number>(
+        MINIMUM_BPM + (seekerLeftPercentage / 100) * (MAXIMUM_BPM - MINIMUM_BPM)
+    )
+    useEffect(() => {
+        const onMouseDown = (e: MouseEvent) => {
+            if (seekBarRef.current?.contains(e.target as Node)) {
+                setIsSeeking(true)
+            }
+        }
+        const onMouseMove = (e: MouseEvent) => {
+            if (isSeeking) {
+                const { current: seekBarRefCurrent } = seekBarRef
+                if (!seekBarRefCurrent) {
+                    return
+                }
+                const { clientWidth, offsetLeft } = seekBarRefCurrent
+                let percentage = ((e.clientX - offsetLeft) / clientWidth) * 100
+                if (percentage < 0) {
+                    percentage = 0
+                }
+                if (percentage > 100) {
+                    percentage = 100
+                }
+
+                setSeekerLeftPercentage(percentage)
+                setBpm(
+                    MINIMUM_BPM +
+                        (percentage / 100) * (MAXIMUM_BPM - MINIMUM_BPM)
+                )
+            }
+        }
+        const onMouseUp = () => {
+            if (isSeeking) {
+                setIsSeeking(false)
+            }
+        }
+        document.addEventListener('mousedown', onMouseDown)
+        document.addEventListener('mousemove', onMouseMove)
+        document.addEventListener('mouseup', onMouseUp)
+
+        return () => {
+            document.removeEventListener('mousedown', onMouseDown)
+            document.removeEventListener('mousemove', onMouseMove)
+            document.removeEventListener('mouseup', onMouseUp)
+        }
+    }, [isSeeking])
 
     return (
         <Layout>
             <ToolBox>
+                <BpmMarker>{Math.ceil(bpm)}</BpmMarker>
                 <SeekBarLayout>
                     <CircleButton>-</CircleButton>
-                    <SeekBar>
-                        <Seeker />
+                    <SeekBar ref={seekBarRef}>
+                        <Seeker
+                            ref={seekerRef}
+                            style={{
+                                left: `calc(${seekerLeftPercentage}% - (16px / 2))`,
+                            }}
+                        />
                     </SeekBar>
                     <CircleButton style={{ marginLeft: 'auto' }}>
                         +
