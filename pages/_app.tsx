d04bb36b2b895a26d7be8acfcaf56895031969d4
yungblud <killi8n@gmail.com>
2022-04-27 19:17:43 +0900

    feat: reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app'
+import { Global, css } from '@emotion/react'
+import emotionReset from 'emotion-reset';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+      <>
+        <Component {...pageProps} />
+        <Global styles={css`
+          ${emotionReset}
+
+          *, *::after, *::before {
+            box-sizing: border-box;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+            font-smoothing: antialiased;
+          }
+        `} />
+      </>
+    )
 }
 
 export default MyApp
