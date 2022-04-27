bea81ebe5e3927d98ee80409872d1e02a319f310
yungblud <killi8n@gmail.com>
2022-04-27 20:36:33 +0900

    feat: added Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 /* eslint-disable jsx-a11y/media-has-caption */
+import styled from '@emotion/styled'
 import type { NextPage } from 'next'
 import { useEffect, useRef } from 'react'
+
+const Layout = styled.main`
+    min-width: 100vw;
+    min-height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const Home: NextPage = () => {
     const audioRef = useRef<HTMLAudioElement>(null)
     useEffect(() => {}, [])
 
     return (
-        <>
+        <Layout>
             <button
                 type="button"
                 onClick={() => {
@@ -19,7 +29,7 @@ const Home: NextPage = () => {
                 Play
             </button>
             <audio ref={audioRef} src="/tick.mp3" />
-        </>
+        </Layout>
     )
 }
 
